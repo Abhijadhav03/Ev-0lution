07301576fc6407068cc3afaaa9a76ae3f6da41f6
Abhijadhav03 <jadhavabhishek53366@gmail.com>
2024-11-01 15:52:03 +0530

    new

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,9 +1,8 @@
-import video1 from '../../Assets/video1.mp4';
 import './Background.css';
 import image1 from '../../Assets/image1.png';
 import image2 from '../../Assets/image2.png';
 import image3 from '../../Assets/image3.png';
-
+import video1 from '../../Assets/video1.mp4';
 const Background = ({ playStatus, heroCount }) => {
     if (playStatus) {
         return (
